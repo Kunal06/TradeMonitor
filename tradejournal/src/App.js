08763c63499294896c65e0e90bfc3b67c60d09df
aildasanik <kunal.aildasani@panevo.com>
2021-06-14 00:07:58 -0700

    Add journal Page

--- tradejournal/src/App.js
+++ tradejournal/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Dashboard from "./pages/Dashboard";
+import DailyJournal from "./pages/DailyJournal";
 import "./sass/styles.scss";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <div className="dark_theme" id="app">
       <Header />
       <Menu />
-      <Dashboard />
+      {/* <Dashboard /> */}
+      <DailyJournal />
     </div>
   );
 }
