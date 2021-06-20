1ddb767428f7d738709d19f68b7091c2d6eef010
aildasanik <kunal.aildasani@panevo.com>
2021-06-20 12:59:20 -0700

    Journal form created with quill comment

--- tradejournal/src/App.js
+++ tradejournal/src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Dashboard from "./pages/Dashboard";
@@ -11,6 +12,7 @@ function App() {
       <Menu />
       {/* <Dashboard /> */}
       <DailyJournal />
+      <Footer />
     </div>
   );
 }
