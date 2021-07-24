4867768813608012304bf211b35ceadf5999dfc2
aildasanik <kunal.aildasani@panevo.com>
2021-07-24 16:59:26 -0700

    FIlter posts with date range

--- tradejournal/src/components/Calendar/index.js
+++ tradejournal/src/components/Calendar/index.js
@@ -6,31 +6,44 @@ import "react-calendar/dist/Calendar.css";
 import { ReactComponent as CalendarIcon } from "../../assets/calendar.svg";
 import useDetectOutsideClick from "../../hooks/useDetectOutsideClick";
 
-const CalendarInput = ({ value, onChange }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const inputRef = useRef(null);
+const CalendarInput = ({ value, onChange, ...otherProps }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const inputRef = useRef(null);
 
-  useDetectOutsideClick(inputRef, setIsOpen);
+    useDetectOutsideClick(inputRef, setIsOpen);
 
-  useEffect(() => {
-    setIsOpen(false);
-  }, [value]);
+    const transformDate = () => {
+        if (Array.isArray(value) && value.length > 1)
+            return (
+                value[0].toLocaleDateString() +
+                " - " +
+                value[1].toLocaleDateString()
+            );
+        else return value.toLocaleDateString();
+    };
 
-  return (
-    <div className="calendar" ref={inputRef}>
-      <div className="calendar_value" onClick={() => setIsOpen(!isOpen)}>
-        <CalendarIcon className="icon-small" />
-        {value.toLocaleDateString()}
-      </div>
-      <Calendar
-        onChange={onChange}
-        value={value}
-        className={
-          isOpen ? `calendar_input calendar_input--open` : "calendar_input"
-        }
-      />
-    </div>
-  );
+    useEffect(() => {
+        setIsOpen(false);
+    }, [value]);
+
+    return (
+        <div className="calendar" ref={inputRef}>
+            <div className="calendar_value" onClick={() => setIsOpen(!isOpen)}>
+                <CalendarIcon className="icon-small" />
+                {transformDate()}
+            </div>
+            <Calendar
+                onChange={onChange}
+                value={value}
+                {...otherProps}
+                className={
+                    isOpen
+                        ? `calendar_input calendar_input--open`
+                        : "calendar_input"
+                }
+            />
+        </div>
+    );
 };
 
 export default CalendarInput;
