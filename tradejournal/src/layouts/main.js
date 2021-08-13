fb931217c4c3b567dfc7d357dd022bb3af000378
aildasanik <kunal.aildasani@panevo.com>
2021-08-13 01:45:25 -0700

    Add page transition animations

--- tradejournal/src/layouts/main.js
+++ tradejournal/src/layouts/main.js
@@ -1,19 +1,37 @@
-import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Menu from "../components/Menu";
+import { motion } from "framer-motion";
+
 const MainLayout = (props) => {
-  const { children, title } = props;
-  return (
-    <>
-      <Header />
-      <Menu />
-      <div className="container">
-        <h3 className="page_title">{title}</h3>
-        <div className="content">{children}</div>
-        <Footer />
-      </div>
-    </>
-  );
+    const { children, title } = props;
+    const pageVariants = {
+        initial: {
+            opacity: 0,
+        },
+        in: {
+            opacity: 1,
+        },
+        out: {
+            opacity: 0,
+        },
+    };
+    return (
+        <>
+            <Header />
+            <Menu />
+            <motion.div
+                className="container"
+                animate="in"
+                initial="initial"
+                exit="out"
+                variants={pageVariants}
+            >
+                <h3 className="page_title">{title}</h3>
+                <div className="content">{children}</div>
+            </motion.div>
+            <Footer />
+        </>
+    );
 };
 export default MainLayout;
